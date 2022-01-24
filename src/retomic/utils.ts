2754d73df465370d5f6272687c9cf39678d2db94
leland-kwong <leland.kwong@gmail.com>
2022-01-23 21:45:49 -0800

    add jsdoc

--- src/retomic/utils.ts
+++ src/retomic/utils.ts
@@ -24,6 +24,13 @@ function shallowCompare(cache: any, value: any) {
   return cache !== value
 }
 
+/**
+ * Returns a new function that compares the old return value
+ * and new return value. If they are the same, then it will
+ * return what was previously returned. This is useful for
+ * determining if two different objects are equal to prevent
+ * unecessary rerenders.
+ */
 export function useIsNew<X, Y>(
   fn: (x: X) => Y,
   isNewValue: (prev: Y, next: Y) => boolean = shallowCompare
