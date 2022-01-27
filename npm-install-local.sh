79f1c3a61f6d5738892d4107d20ede99e17f3dc9
leland-kwong <leland.kwong@gmail.com>
2022-01-27 00:42:19 -0800

    improve local npm install script

--- npm-install-local.sh
+++ npm-install-local.sh
@@ -1,4 +1,12 @@
 # packs and installs locally to the provided directory
-npm pack --pack-destination $1\
+
+cleanfiles() {
+  ls | sed -n '/^retomic./p' | xargs rm
+}
+
+# cleanup tar files before and after installing
+cleanfiles\
+  && npm pack --pack-destination $1\
   && cd $1\
-  && npm install retomic*.tgz
+  && npm install retomic*.tgz\
+  && cleanfiles
